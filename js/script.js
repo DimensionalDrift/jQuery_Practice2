71eea31ef226577ffdc87f67474723fda6fe56e0
Chris Anderson <traveller.anderson@gmail.com>
2019-02-25 22:04:47 +0000

    Finished Challenge 2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
- $("#button1").mouseenter(function(){
+ $('#button1').mouseenter(function(){
 		$(this).removeClass("makeRed").addClass("makeBorder");
 	});
 
-	$("#button1").mouseleave(function(){
+	$('#button1').mouseleave(function(){
 		$(this).removeClass("makeBorder").addClass("makeRed");
+	});
+
+	$('#button1').click(function(){
+		$('#para1').slideToggle()
 	});
 
 }); 
